e27ae1533ba8abb72bcfb84e498853dc37794a0a
aryannP03 <arpatel@trueigtech.com>
2026-02-11 13:10:09 +0530

    add task button reposition

--- src/common/filter/Filter.jsx
+++ src/common/filter/Filter.jsx
@@ -4,7 +4,7 @@ function Filter({ priority, setPriority}) {
     return (
         <div>
             <label className="filter-task text-white text-xl" >Filter : </label>
-                <select className="text-white border rounded-lg bg-card-bg" value={priority} onChange= {(e) => setPriority(e.target.value)}>
+                <select className="text-white text-lg border rounded-lg bg-card-bg px-3 py-1" value={priority} onChange= {(e) => setPriority(e.target.value)}>
                     <option value="all">All</option>
                     <option value="low">Low</option>
                     <option value="medium">Medium</option>
--- src/common/search/Search.jsx
+++ src/common/search/Search.jsx
@@ -5,9 +5,9 @@ function Searchtask( { searchvalue, setSearchValue } ) {
     return (
         <div className='search-task text-white text-xl'>
             <label>Search : </label>
-            <input type="text" value={searchvalue}
+            <input type="text" value={searchvalue} placeholder="Search task"
                 onChange={(e) => setSearchValue(e.target.value)}  
-                className="border-2 border-white rounded-lg"/>
+                className="border-2 border-white rounded-lg p-1"/>
         </div>
     )
 }
